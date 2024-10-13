8e900b9dd281ce12c1f8c03e3e1c78e65219fe39
Kalu David <kaludavid411@gmail.com>
2024-10-13 23:24:40 +0100

    first commit

---
+++ rap-names-api/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const rappers = {
+    '21 savage':{
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'    
+    },
+    'chance the rapper':{
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'    
+    },
+    'dylan':{
+        'age': 29,
+        'birthName': 'Dylan',
+        'birthLocation': 'Dylan'    
+    }
+}
+
+app.get('/', (req, res)=> {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (req, res)=>{
+    const rappersName = req.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+        res.json(rappers[rappersName])
+    }else{
+        res.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running on ${PORT}! You better go catch it!`)
+})
